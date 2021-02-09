95994240d3d890071b0c824a1f89e84c6cbd3ee1
wu-ToStar <2135250352@qq.com>
2021-02-09 23:40:44 +0800

    Update HelloWorld.vue

--- Vue/Test/UserVuex/src/components/HelloWorld.vue
+++ Vue/Test/UserVuex/src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
-    {{msg}} <div></div>
-    {{$store.state.count}}
+    {{ msg }}
+    <div></div>
+    {{ $store.state.count }}
     <button @click="up">+</button>
     <button @click="down">-</button>
   </div>
@@ -9,23 +10,22 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App",
+    };
   },
-  methods:{
-    up(){
-      this.$store.commit('increment')
+  methods: {
+    up() {
+      this.$store.commit("increment");
     },
-    down(){
-      this.$store.commit('decrement')
-    }
-  }
-}
+    down() {
+      this.$store.commit("decrement");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
