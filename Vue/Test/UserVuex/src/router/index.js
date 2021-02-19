2614f6a8c4bc5ec7371e468fae6a89709fbdc2e5
wu-ToStar <2135250352@qq.com>
2021-02-19 21:14:42 +0800

    Update index.js

--- Vue/Test/UserVuex/src/router/index.js
+++ Vue/Test/UserVuex/src/router/index.js
@@ -3,9 +3,8 @@ import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
 
 Vue.use(Router);
-
 export default new Router({
-  
+
   routes: [{
     path: "/",
     name: "HelloWorld",
